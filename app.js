90aef4c0d0b86cb48bd11593c404006073806b1f
lev-kazakov <lev.kazakov@gmail.com>
2014-02-09 01:13:50 +0200

    renamed and re-arranged files

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.post('/register', function (req, res, next) {
     }
 });
 
-app.use(miniexpress.static(__dirname + "/app"));
+app.use(miniexpress.static(__dirname + "/static"));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
